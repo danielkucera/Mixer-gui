ff7dc60a022fca3ef2e1bd3dc2dd1788a48f7ee0
Daniel Kucera <daniel.kucera@gmail.com>
2013-12-28 11:13:47 +0100

    added fps and clock

--- buffer.cpp
+++ buffer.cpp
@@ -2,11 +2,12 @@
 #include "stdlib.h"
 #include "stdio.h"
 
-Buffer::Buffer(int widt, int heigh, int bp)
+Buffer::Buffer(int widt, int heigh, int bp, int fp)
 {
     width=widt;
     height=heigh;
     bpp=bp;
+    fps=fp;
 
     buf_len = width*height*bpp;
 
@@ -15,6 +16,10 @@ Buffer::Buffer(int widt, int heigh, int bp)
     for (int i=0; i<32; i++){
         isopen[i]=0;
     }
+
+    clock = new QTimer(this);
+    clock->start(1000/fps);
+
 }
 
 void* Buffer::Open(int number){
@@ -22,7 +27,7 @@ void* Buffer::Open(int number){
     fprintf(stderr, "Opening %d\n",number);
 
     if (isopen[number]==0){
-        fprintf(stderr, "Trying malloc for %d with buf_len %d\n",number, buf_len);
+        //fprintf(stderr, "Trying malloc for %d with buf_len %d\n",number, buf_len);
         start[number] = malloc(buf_len);
         memset(start[number],0,buf_len);
     }
--- buffer.h
+++ buffer.h
@@ -3,26 +3,30 @@
 
 #include <QObject>
 #include <cstring>
+#include <QTimer>
 
 class Buffer : public QObject
 {
     Q_OBJECT
 public:
-    Buffer(int widt, int heigh, int bp);
+    Buffer(int widt, int heigh, int bp, int fps);
 
     void* Open(int number);
 
     void* start[32];
     int frame[32]={0};
-    int fps;
     int width;
     int height;
     int bpp;
+    int fps;
     int buf_len;
     void newFrame(int number);
 
+    QTimer *clock;
+
 private:
     int isopen[32];
+
 
 signals:
     void newFrameSignal(int number);
